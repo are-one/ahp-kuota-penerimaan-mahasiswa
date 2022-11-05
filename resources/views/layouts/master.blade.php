5f44aac99faa6e6ab5f7244f2442d07b2863c580
Are1PM <arwanpriantomangidi@gmail.com>
2022-11-05 12:33:26 +0800

    [UPDATE] ubah posisi menu

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -65,9 +65,9 @@
                     <a href="/" class="nav-item nav-link"><i class="fa fa-home me-2"></i>Home</a>
                     <a href="/prodi" class="nav-item nav-link"><i class="fa fa-laptop me-2"></i>Data Prodi</a>
                     <a href="/kriteria" class="nav-item nav-link"><i class="fas fa-hotel me-2"></i>Kriteria</a>
+                    <a href="/tahun_akademik" class="nav-item nav-link"><i class="fa fa-calendar me-2"></i>Tahun Akademik</a>
                     <a href="/pembobotan" class="nav-item nav-link"><i class="fa fa-keyboard me-2"></i>Data Bobot</a>
                     <a href="/hasil_penilaian" class="nav-item nav-link"><i class="fa fa-table me-2"></i>Hasil Penilaian</a>
-                    <a href="/tahun_akademik" class="nav-item nav-link"><i class="fa fa-calendar me-2"></i>Tahun Akademik</a>
                     <a href="{{ route('logout') }}" class="nav-item nav-link" onclick="event.preventDefault();
                         document.getElementById('logout-form').submit();">
                         <i class="fas fa-sign-out-alt"></i> {{ __('Logout') }}
